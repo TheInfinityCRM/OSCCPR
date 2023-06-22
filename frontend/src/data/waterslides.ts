1d482c046e77e2e2c810536b11f3f5186ed8b1e3
Frenchfry255 <124072299+Frenchfry255@users.noreply.github.com>
2023-06-22 19:41:08 -0400

    Update waterslides.ts

--- frontend/src/data/waterslides.ts
+++ frontend/src/data/waterslides.ts
@@ -6,6 +6,6 @@ import tropical4 from '@assets/imgs/tropical_slide4.jpeg'
 export const homeSlides = [
     {id:1, Image:tropical1, alt:'Slide 1', title:'Tropical Double Waterslide', oneDayRent:'$500.00', twoDayRent:'$700.00', WeekendRent:'$700.00'},
     {id:2, Image:tropical2, alt:'Slide 2', title:'Tropical Inferno Waterslide', oneDayRent:'$300.00', twoDayRent:'$500.00', WeekendRent:'$650.00'},
-    {id:3, Image:tropical3, alt:'Slide 3', title:'Castle Bounce House/Waterslide', oneDayRent:'$500.00', twoDayRent:'$700.00', WeekendRent:'$650.00'},
-    {id:4, Image:tropical4, alt:'Slide 4', title:'Toddler Bounce House/Slide', oneDayRent:'$200.00', twoDayRent:'$300.00', WeekendRent:'$400.00'},
+    {id:3, Image:tropical3, alt:'Slide 3', title:'Castle Bounce House/Waterslide', oneDayRent:'$200.00', twoDayRent:'$300.00', WeekendRent:'$40.00'},
+    {id:4, Image:tropical4, alt:'Slide 4', title:'Toddler Bounce House/Slide', oneDayRent:'$100.00', twoDayRent:'$150.00', WeekendRent:'$200.00'},
 ]
