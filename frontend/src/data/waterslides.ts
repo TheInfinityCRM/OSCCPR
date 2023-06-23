499a64c0d7effc0dc5b542a36a781739048e198f
Frenchfry255 <124072299+Frenchfry255@users.noreply.github.com>
2023-06-22 20:55:54 -0400

    Update waterslides.ts

--- frontend/src/data/waterslides.ts
+++ frontend/src/data/waterslides.ts
@@ -4,7 +4,7 @@ import tropical3 from '@assets/imgs/tropical_slide3.png'
 import tropical4 from '@assets/imgs/tropical_slide4.png'
 
 export const homeSlides = [
-    {id:1, Image:tropical1, alt:'Slide 1', title:'Ice Luge', oneDayRent:'$50.00', twoDayRent:'$65.00'},
+    {id:1, Image:tropical1, alt:'Slide 1', title:'Ice Luge', oneDayRent:'$50.00', withled:'$65.00'},
     {id:2, Image:tropical2, alt:'Slide 2', title:'Tropical Inferno Waterslide', oneDayRent:'$300.00', twoDayRent:'$500.00'},
     {id:3, Image:tropical3, alt:'Slide 3', title:'Castle Bounce House/Waterslide', oneDayRent:'$200.00', twoDayRent:'$300.00'},
     {id:4, Image:tropical4, alt:'Slide 4', title:'Toddler Bounce House/Slide', oneDayRent:'$100.00', twoDayRent:'$150.00'},
