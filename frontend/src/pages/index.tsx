46232c435548f8ef1b8dc1c517c9c47a9d5a6e05
Frenchfry255 <124072299+Frenchfry255@users.noreply.github.com>
2023-06-22 20:10:08 -0400

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -125,7 +125,7 @@ const Home: NextPageWithLayout = () => {
           </div>
           <div id="water slides and bounce houses">
             <div className="flex justify-center text-center items-center py-6">
-              <Heading title="Water Slides &amp; Bounce Houses" />
+              <Heading title="Inflatables &amp; More" />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-6">
               {homeSlides.map((value) => (
